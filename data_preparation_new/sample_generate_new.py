27c581fc253a4486284faccdc2cdcfe6446054ba
alex.schein <alex.schein@noaa.gov>
2026-06-08 18:43:18 +0000

    Added remaining files

---
+++ data_preparation_new/sample_generate_new.py
@@ -0,0 +1,188 @@
+import argparse
+import os
+import sys
+import datetime as dt
+
+# Data Science Core
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+# Geospatial & Analysis
+# import xesmf
+# import cartopy.crs as ccrs
+# import cartopy.feature as cfeature
+# from scipy.spatial import KDTree
+# from metpy.calc import specific_humidity_from_dewpoint
+# from metpy.units import units
+
+# Plotting
+# import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
+# from matplotlib import colors
+# from matplotlib.markers import MarkerStyle
+
+# Custom Packages
+# from nnja_ai import DataCatalog
+from funcs_modified import *
+
+###############################
+
+# (2026-05-07) barebones for now but will get more added in when satellite issues are solved
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--starting_analysis_time", type=str) #Must be formatted as "YYYY-MM-DD_HH"
+parser.add_argument("--ending_analysis_time", type=str) #Must be formatted as "YYYY-MM-DD_HH"
+parser.add_argument("--save_directory", type=str, default=None)
+
+
+args = parser.parse_args()
+starting_analysis_time = dt.datetime.strptime(args.starting_analysis_time, "%Y-%m-%d_%H")
+ending_analysis_time = dt.datetime.strptime(args.ending_analysis_time, "%Y-%m-%d_%H")
+save_directory = args.save_directory
+
+if save_directory is None:
+   sys.exit("ERROR: --save_directory must be specifed!")
+   
+
+analysis_times_list = pd.date_range(start=starting_analysis_time, end=ending_analysis_time, freq='h').to_pydatetime().tolist()
+
+
+### Vars shared between HRRR/RTMA and sta
+adaf_var_list = ['sp', 't', 'q', 'u10', 'v10']
+
+adaf_grid_spacing = 0.05
+adaf_lats = (np.arange(24.7, 50.25+adaf_grid_spacing, adaf_grid_spacing)).round(4)
+adaf_lons = (np.arange(232, 295.95+adaf_grid_spacing, adaf_grid_spacing)).round(4)
+ds_adaf_lats_lons = xr.Dataset(coords={"lat": adaf_lats, "lon": adaf_lons})
+df_adaf_lats_lons = (pd.MultiIndex.from_product([adaf_lats, adaf_lons], names=["lat","lon"])).to_frame(index=False)
+
+stats_filepath=f"/scratch3/BMC/wrfruc/aschein/ADAF/data_preparation_new/stats.csv"
+stats = pd.read_csv(stats_filepath, index_col=0)
+
+### HRRR/RTMA specific vars, static
+hrrr_forecast_leadtime = 1
+
+rtma_variables = [f"rtma_{x}" for x in adaf_var_list] 
+hrrr_variables = [f"hrrr_{x}" for x in adaf_var_list] 
+
+#Static terrain field
+topo = get_grid_alt_and_regrid(f"/scratch3/BMC/wrfruc/aschein/Train_Test_Files/terrain_CONUS_URMA_2p5km.grib2", ds_adaf_lats_lons)
+topo_normed = min_max_norm_ignore_extreme_fill_nan_onevar_onetime(topo, 'z', stats_filepath)
+
+### Station specific vars, static
+obs_time_window = 3 #hours
+time_period = 14 #days
+catalog_str = 'conv-adpsfc-NC000007'
+list_of_metar_vars = ["LAT","LON", "OBS_TIMESTAMP", "MTRTMP.TMDB", "MTRTMP.TMDP", "MTRPRS.ALSE", "MTRWND.WSPD", "MTRWND.WDIR"]
+
+#Initialize outside the main loop
+df_master = get_nnja_metar_dataframe(analysis_times_list[0], list_of_metar_vars, time_period=time_period, catalog_str=catalog_str)
+
+written_count = 0
+already_exists_count = 0
+skipped_missing_obs_count = 0
+skipped_empty_sta_count = 0
+
+for t, analysis_time in enumerate(analysis_times_list):
+    output_filename = f"{analysis_time.strftime("%Y-%m-%d_%H")}.nc"
+    if os.path.exists(f"{save_directory}/{output_filename}"):
+        print(f"{output_filename} already exists in {save_directory}")
+        already_exists_count += 1
+    else:
+        hrrr_init_time = analysis_time - dt.timedelta(hours=hrrr_forecast_leadtime) #need to call the proper f01 HRRR file
+        
+        # Dynamic file directories 
+        hrrr_directory = f"/scratch5/BMC/ai-datadepot/data/models/hrrr/conus/grib2/{hrrr_init_time.strftime("%Y%m%d")}"
+        rtma_directory=f"/scratch5/BMC/ai-datadepot/data/models/rtma/2p5km/grib2/{analysis_time.strftime("%Y%m%d")}" #2026-05-29 updated to the main depot
+    
+        ### Generate all RTMA, HRRR fields
+        hrrr_data = []
+        rtma_data = []
+        
+        for i, adaf_var in enumerate(adaf_var_list):
+        
+            hrrr_filename = f"hrrr.t{str(hrrr_init_time.hour).zfill(2)}z.wrfnatf01.grib2"
+            rtma_filename = f"rtma2p5.t{str(analysis_time.hour).zfill(2)}z.2dvaranl_ndfd.grb2_wexp"
+
+            xr_hrrr_regridded, xr_rtma_regridded = fetch_and_regrid_hrrr_rtma(adaf_var, 
+                                                                              ds_adaf_lats_lons,
+                                                                              hrrr_filepath=f"{hrrr_directory}/{hrrr_filename}", 
+                                                                              rtma_filepath=f"{rtma_directory}/{rtma_filename}")      
+        
+            xr_hrrr_fixed = fix_dataset_scaling_shifting(xr_hrrr_regridded, adaf_var)
+            xr_rtma_fixed = fix_dataset_scaling_shifting(xr_rtma_regridded, adaf_var)
+        
+            xr_rtma_normed = min_max_norm_ignore_extreme_fill_nan_onevar_onetime(xr_rtma_fixed, 
+                                                                                rtma_variables[i], 
+                                                                                stats_filepath)
+        
+            xr_hrrr_normed = min_max_norm_ignore_extreme_fill_nan_onevar_onetime(xr_hrrr_fixed, 
+                                                                                hrrr_variables[i], 
+                                                                                stats_filepath)
+        
+            hrrr_data.append(xr_hrrr_normed.data)
+            rtma_data.append(xr_rtma_normed.data)
+    
+        ds_hrrr_rtma = xr.Dataset(
+                {
+                # %% rtma, normalized
+                f"{rtma_variables[0]}": (("lat", "lon"), rtma_data[0]),
+                f"{rtma_variables[1]}": (("lat", "lon"), rtma_data[1]),
+                f"{rtma_variables[2]}": (("lat", "lon"), rtma_data[2]),
+                f"{rtma_variables[3]}": (("lat", "lon"), rtma_data[3]),
+                f"{rtma_variables[4]}": (("lat", "lon"), rtma_data[4]),
+                
+                # %% HRRR, now normalized (as of 2026/4/27)
+                f"{hrrr_variables[0]}": (("lat", "lon"), hrrr_data[0]),
+                f"{hrrr_variables[1]}": (("lat", "lon"), hrrr_data[1]),
+                f"{hrrr_variables[2]}": (("lat", "lon"), hrrr_data[2]),
+                f"{hrrr_variables[3]}": (("lat", "lon"), hrrr_data[3]),
+                f"{hrrr_variables[4]}": (("lat", "lon"), hrrr_data[4]),
+    
+                #topography, normalized
+                f"z" : (("lat", "lon"), topo_normed.data),
+                },
+            coords={
+                "valid_time": analysis_time,
+                "lat": adaf_lats,
+                "lon": adaf_lons 
+                    },
+            )
+    
+        ### Station obs
+        df_master = check_nnja_metar_dataframe(df_master, analysis_time, list_of_metar_vars)
+        missing_obs_hours = get_missing_obs_hours(df_master, analysis_time, obs_time_window=obs_time_window)
+        if len(missing_obs_hours) > 0:
+            missing_str = ", ".join([x.strftime("%Y-%m-%d %H:%M") for x in missing_obs_hours])
+            print(f"Skipping {output_filename}: missing required obs hour(s) for station window -> {missing_str}")
+            skipped_missing_obs_count += 1
+            continue
+
+        df = assemble_complete_metar_df(df_master, analysis_time, obs_time_window, df_adaf_lats_lons)
+        if df.empty:
+            print(f"Skipping {output_filename}: no valid station observations remained after filtering/QC.")
+            skipped_empty_sta_count += 1
+            continue
+
+        df = min_max_norm_ignore_extreme_fill_nan_sta_df(df, stats_path=stats_filepath)
+        ds_sta_obs = create_obs_xarray(df, df_adaf_lats_lons, analysis_time)
+
+        ### Merge and compress and save
+        ds = xr.merge([ds_hrrr_rtma, ds_sta_obs], compat="no_conflicts")
+
+        comp_settings = {"zlib": True, "complevel": 1}
+        encoding = {var: comp_settings for var in ds.data_vars}
+
+        ds.to_netcdf(f"{save_directory}/{output_filename}", encoding=encoding)
+        print(f"{output_filename} saved to {save_directory}")
+        written_count += 1
+
+print(
+    "Run summary: "
+    f"requested={len(analysis_times_list)}, "
+    f"written={written_count}, "
+    f"already_exists={already_exists_count}, "
+    f"skipped_missing_obs_window={skipped_missing_obs_count}, "
+    f"skipped_empty_station_obs={skipped_empty_sta_count}"
+)
---
+++ utils/dataloader_multifiles_exp.py
@@ -0,0 +1,159 @@
+import os
+import glob
+import torch
+import logging
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from torch.utils.data import DataLoader, Dataset, DistributedSampler
+# from torch.utils.data.distributed import DistributedSampler
+
+####################
+def get_data_loader(params, files_location, distributed, train):
+    dataset = GetDataset(params, files_location, train)
+
+    if distributed:
+        sampler = DistributedSampler(dataset, shuffle=train)
+    else:
+        sampler=None
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=int(params.batch_size),
+        num_workers=params.num_data_workers,
+        shuffle=False,  # (sampler is none),
+        sampler=sampler if train else None,
+        drop_last=True,
+        pin_memory=torch.cuda.is_available(),
+    )
+
+    if train:
+        return dataloader, dataset, sampler
+    else:
+        return dataloader, dataset
+
+####################
+
+class GetDataset(Dataset):
+    def __init__(self, params, files_location, train):
+        self.params = params
+        self.train = train
+        self.files_location = files_location
+        self.n_in_channels = params.n_in_channels
+        self.n_out_channels = params.n_out_channels
+        # self.add_noise = params.add_noise if train else false
+        self.get_file_stats()
+
+    ###
+    
+    def get_file_stats(self):
+        self.file_paths = glob.glob(self.files_location + "/*.nc")
+        self.file_paths.sort()
+        self.num_samples_total = len(self.file_paths)
+    
+        print(f"Getting file stats from {self.file_paths[0]}")
+        ds = xr.open_dataset(self.file_paths[0])
+    
+        # Reversed from original ADAF code - we need x to be lon and y to be lat
+        self.org_img_shape_x = ds["hrrr_t"].shape[1]
+        self.org_img_shape_y = ds["hrrr_t"].shape[0]
+        ds.close()
+
+    ###
+
+    def open_file(self, hour_idx):
+        return xr.open_dataset(self.file_paths[hour_idx])
+
+    ###
+
+    def __len__(self):
+        return self.num_samples_total
+
+    ###
+
+    def __getitem__(self, hour_idx):
+        with self.open_file(hour_idx) as ds:
+
+            #Load lons, lats, topography
+            lon = np.array(ds.coords["lon"].values)[:self.params.img_size_x]
+            lat = np.array(ds.coords["lat"].values)[:self.params.img_size_y]
+            topo = np.array(ds[["z"]].to_array())[:, : self.params.img_size_y, : self.params.img_size_x]
+        
+            #Load HRRR fields
+            if len(self.params.inp_hrrr_vars) != 0:
+                inp_hrrr = np.array(ds[self.params.inp_hrrr_vars].to_array())[:, :self.params.img_size_y, :self.params.img_size_x]
+                inp_hrrr = np.squeeze(inp_hrrr)
+
+                # Create field mask: 1 where data is valid (non-zero), 0 where invalid (zero)
+                field_mask = inp_hrrr.copy()
+                field_mask[field_mask != 0] = 1
+
+            #Load obs
+            if len(self.params.inp_obs_vars) != 0:
+                obs = np.array(ds[self.params.inp_obs_vars].to_array())[
+                    :, -self.params.obs_time_window:, :self.params.img_size_y, :self.params.img_size_x]
+
+                #Get most recent obs as target
+                obs_tar = obs[:, -1]
+
+                ## Quality control - commented out because this should be done in the dataset generation script, so doing it again here is pointless overhead, but keeping in for legacy reasons (may need it later, who knows)
+                # obs_tar[(obs_tar <= -1) | (obs_tar >= 1)] = 0
+
+                # Make a mask of the obs - used to replace values in the target (RTMA) field later
+                obs_tar_mask = obs_tar.copy()
+                obs_tar_mask[obs_tar_mask != 0] = 1
+
+                #Hold out obs; note the held-out obs are still used to replace target field values
+                if self.params.hold_out_obs:
+                    if self.params.obs_mask_seed != 0: #use a set seed; if 0, then use a random seed
+                        np.random.seed(self.params.obs_mask_seed)
+
+                    obs_flattened = obs[0,0].flatten()
+                    obs_indices = np.where(obs_flattened != 0)[0]
+
+                    hold_out_num = int(len(obs_indices) * self.params.hold_out_obs_ratio)
+
+                    np.random.shuffle(obs_indices)
+                    hold_out_obs_indices = obs_indices[:hold_out_num] #pluck out every Nth point
+
+                    #Make the mask without the held out obs
+                    obs_mask = np.zeros(np.shape(obs_flattened))
+                    obs_mask[hold_out_obs_indices] = 1
+                    obs_mask = obs_mask.reshape(obs[0,0].shape[0], obs[0,0].shape[1])
+
+                    #Final input obs = obs minus held out obs
+                    inp_obs = obs*(1-obs_mask)
+                    inp_obs = inp_obs.reshape((-1, self.params.img_size_y, self.params.img_size_x)) #not sure if this is needed...
+
+        #####
+        ## Satellite stuff here, when done
+        #####
+
+            #Load target (RTMA) fields
+            field_tar = np.array(ds[self.params.field_tar_vars].to_array())[:, : self.params.img_size_y, : self.params.img_size_x]
+
+            #Replace target field with obs @ observed locations (all obs locations, including those held out previously)
+            field_obs_tar = field_tar.copy()
+            field_obs_tar[obs_tar_mask == 1] = 0
+            field_obs_tar += obs_tar
+
+            if self.params.learn_residual:
+                field_tar = field_tar - inp_hrrr
+                obs_tar = obs_tar - inp_hrrr
+                field_obs_tar = field_obs_tar - inp_hrrr
+
+            #Make final input tensor
+            inp = np.concatenate((inp_hrrr, inp_obs, topo), axis=0)
+            #Satellite version here when that's done
+
+        return (inp,
+                field_tar,
+                obs_tar,
+                field_obs_tar,
+                inp_hrrr,
+                lat,
+                lon,
+                field_mask,
+                obs_tar_mask)
+                
