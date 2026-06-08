0d708429290180363b5aa281a651a7d1a96173c7
alex.schein <alex.schein@noaa.gov>
2026-06-08 22:08:25 +0000

    Removed old test files, merged working new ones into main workflow

--- utils/dataloader_multifiles.py
+++ utils/dataloader_multifiles.py
@@ -22,6 +22,7 @@ def get_data_loader(params, files_location, distributed, train):
         dataset,
         batch_size=int(params.batch_size),
         num_workers=params.num_data_workers,
+        prefetch_factor=1,
         shuffle=False,  # (sampler is none),
         sampler=sampler if train else None,
         drop_last=True,
@@ -58,14 +59,12 @@ class GetDataset(Dataset):
         # Reversed from original ADAF code - we need x to be lon and y to be lat
         self.org_img_shape_x = ds["hrrr_t"].shape[1]
         self.org_img_shape_y = ds["hrrr_t"].shape[0]
-    
-        self.files = [None for _ in range(self.num_samples_total)]
+        ds.close()
 
     ###
 
     def open_file(self, hour_idx):
-        file = xr.open_dataset(self.file_paths[hour_idx])
-        self.files[hour_idx] = file
+        return xr.open_dataset(self.file_paths[hour_idx])
 
     ###
 
@@ -75,80 +74,79 @@ class GetDataset(Dataset):
     ###
 
     def __getitem__(self, hour_idx):
-        if self.files[hour_idx] is None: #hasn't yet been loaded
-            self.open_file(hour_idx)
+        with self.open_file(hour_idx) as ds:
 
-        #Load lons, lats, topography
-        lon = np.array(self.files[hour_idx].coords["lon"].values)[:self.params.img_size_x]
-        lat = np.array(self.files[hour_idx].coords["lat"].values)[:self.params.img_size_y]
-        topo = np.array(self.files[hour_idx][["z"]].to_array())[:, : self.params.img_size_y, : self.params.img_size_x]
+            #Load lons, lats, topography
+            lon = np.array(ds.coords["lon"].values)[:self.params.img_size_x]
+            lat = np.array(ds.coords["lat"].values)[:self.params.img_size_y]
+            topo = np.array(ds[["z"]].to_array())[:, : self.params.img_size_y, : self.params.img_size_x]
         
-        #Load HRRR fields
-        if len(self.params.inp_hrrr_vars) != 0:
-            inp_hrrr = np.array(self.files[hour_idx][self.params.inp_hrrr_vars].to_array())[:, :self.params.img_size_y, :self.params.img_size_x]
-            inp_hrrr = np.squeeze(inp_hrrr)
+            #Load HRRR fields
+            if len(self.params.inp_hrrr_vars) != 0:
+                inp_hrrr = np.array(ds[self.params.inp_hrrr_vars].to_array())[:, :self.params.img_size_y, :self.params.img_size_x]
+                inp_hrrr = np.squeeze(inp_hrrr)
 
-            # Create field mask: 1 where data is valid (non-zero), 0 where invalid (zero)
-            field_mask = inp_hrrr.copy()
-            field_mask[field_mask != 0] = 1
+                # Create field mask: 1 where data is valid (non-zero), 0 where invalid (zero)
+                field_mask = inp_hrrr.copy()
+                field_mask[field_mask != 0] = 1
 
-        #Load obs
-        if len(self.params.inp_obs_vars) != 0:
-            obs = np.array(self.files[hour_idx][self.params.inp_obs_vars].to_array())[
-                :, -self.params.obs_time_window:, :self.params.img_size_y, :self.params.img_size_x]
+            #Load obs
+            if len(self.params.inp_obs_vars) != 0:
+                obs = np.array(ds[self.params.inp_obs_vars].to_array())[
+                    :, -self.params.obs_time_window:, :self.params.img_size_y, :self.params.img_size_x]
 
-            #Get most recent obs as target
-            obs_tar = obs[:, -1]
+                #Get most recent obs as target
+                obs_tar = obs[:, -1]
 
-            ## Quality control - commented out because this should be done in the dataset generation script, so doing it again here is pointless overhead, but keeping in for legacy reasons (may need it later, who knows)
-            # obs_tar[(obs_tar <= -1) | (obs_tar >= 1)] = 0
+                ## Quality control - commented out because this should be done in the dataset generation script, so doing it again here is pointless overhead, but keeping in for legacy reasons (may need it later, who knows)
+                # obs_tar[(obs_tar <= -1) | (obs_tar >= 1)] = 0
 
-            # Make a mask of the obs - used to replace values in the target (RTMA) field later
-            obs_tar_mask = obs_tar.copy()
-            obs_tar_mask[obs_tar_mask != 0] = 1
+                # Make a mask of the obs - used to replace values in the target (RTMA) field later
+                obs_tar_mask = obs_tar.copy()
+                obs_tar_mask[obs_tar_mask != 0] = 1
 
-            #Hold out obs; note the held-out obs are still used to replace target field values
-            if self.params.hold_out_obs:
-                if self.params.obs_mask_seed != 0: #use a set seed; if 0, then use a random seed
-                    np.random.seed(self.params.obs_mask_seed)
+                #Hold out obs; note the held-out obs are still used to replace target field values
+                if self.params.hold_out_obs:
+                    if self.params.obs_mask_seed != 0: #use a set seed; if 0, then use a random seed
+                        np.random.seed(self.params.obs_mask_seed)
 
-                obs_flattened = obs[0,0].flatten()
-                obs_indices = np.where(obs_flattened != 0)[0]
+                    obs_flattened = obs[0,0].flatten()
+                    obs_indices = np.where(obs_flattened != 0)[0]
 
-                hold_out_num = int(len(obs_indices) * self.params.hold_out_obs_ratio)
+                    hold_out_num = int(len(obs_indices) * self.params.hold_out_obs_ratio)
 
-                np.random.shuffle(obs_indices)
-                hold_out_obs_indices = obs_indices[:hold_out_num] #pluck out every Nth point
+                    np.random.shuffle(obs_indices)
+                    hold_out_obs_indices = obs_indices[:hold_out_num] #pluck out every Nth point
 
-                #Make the mask without the held out obs
-                obs_mask = np.zeros(np.shape(obs_flattened))
-                obs_mask[hold_out_obs_indices] = 1
-                obs_mask = obs_mask.reshape(obs[0,0].shape[0], obs[0,0].shape[1])
+                    #Make the mask without the held out obs
+                    obs_mask = np.zeros(np.shape(obs_flattened))
+                    obs_mask[hold_out_obs_indices] = 1
+                    obs_mask = obs_mask.reshape(obs[0,0].shape[0], obs[0,0].shape[1])
 
-                #Final input obs = obs minus held out obs
-                inp_obs = obs*(1-obs_mask)
-                inp_obs = inp_obs.reshape((-1, self.params.img_size_y, self.params.img_size_x)) #not sure if this is needed...
+                    #Final input obs = obs minus held out obs
+                    inp_obs = obs*(1-obs_mask)
+                    inp_obs = inp_obs.reshape((-1, self.params.img_size_y, self.params.img_size_x)) #not sure if this is needed...
 
         #####
         ## Satellite stuff here, when done
         #####
 
-        #Load target (RTMA) fields
-        field_tar = np.array(self.files[hour_idx][self.params.field_tar_vars].to_array())[:, : self.params.img_size_y, : self.params.img_size_x]
+            #Load target (RTMA) fields
+            field_tar = np.array(ds[self.params.field_tar_vars].to_array())[:, : self.params.img_size_y, : self.params.img_size_x]
 
-        #Replace target field with obs @ observed locations (all obs locations, including those held out previously)
-        field_obs_tar = field_tar.copy()
-        field_obs_tar[obs_tar_mask == 1] = 0
-        field_obs_tar += obs_tar
+            #Replace target field with obs @ observed locations (all obs locations, including those held out previously)
+            field_obs_tar = field_tar.copy()
+            field_obs_tar[obs_tar_mask == 1] = 0
+            field_obs_tar += obs_tar
 
-        if self.params.learn_residual:
-            field_tar = field_tar - inp_hrrr
-            obs_tar = obs_tar - inp_hrrr
-            field_obs_tar = field_obs_tar - inp_hrrr
+            if self.params.learn_residual:
+                field_tar = field_tar - inp_hrrr
+                obs_tar = obs_tar - inp_hrrr
+                field_obs_tar = field_obs_tar - inp_hrrr
 
-        #Make final input tensor
-        inp = np.concatenate((inp_hrrr, inp_obs, topo), axis=0)
-        #Satellite version here when that's done
+            #Make final input tensor
+            inp = np.concatenate((inp_hrrr, inp_obs, topo), axis=0)
+            #Satellite version here when that's done
 
         return (inp,
                 field_tar,
