ba653472ed4c721aa4fa7785860d08f940656238
alex.schein <alex.schein@noaa.gov>
2026-06-11 23:00:01 +0000

    Initial test of using a separate parameters file

--- utils/YParams.py
+++ utils/YParams.py
@@ -1,20 +1,14 @@
-# import os
-# import sys
-import logging
+import argparse
 from ruamel.yaml import YAML
 
 
 class YParams:
     """Yaml file parser"""
 
-    def __init__(self, yaml_filename, config_name, print_params=False):
+    def __init__(self, yaml_filename, config_name="EncDec", print_params=False):
         self._yaml_filename = yaml_filename
-        self._config_name = config_name
+        self._config_name = config_name #(2026-06-11) Currently only "EncDec" is supported, but leaving this functionality here in case it's ever useful
         self.params = {}
-
-        # if print_params:
-        #     print(os.system('hostname'))
-        # print("Configuration:", yaml_filename)
 
         with open(yaml_filename, "rb") as _file:
             yaml = YAML().load(_file)
@@ -42,10 +36,17 @@ class YParams:
             self.params[key] = val
             self.__setattr__(key, val)
 
-    def log(self):
-        logging.info("------------------ Configuration ------------------")
-        logging.info("Configuration file: " + str(self._yaml_filename))
-        logging.info("Configuration name: " + str(self._config_name))
-        for key, val in self.params.items():
-            logging.info(str(key) + ": " + str(val))
-        logging.info("---------------------------------------------------")
+    def override_from_cli(self, args):
+        """
+        Intercepts command-line arguments and overrides matching YAML parameters.
+        Ignored if the CLI argument is None or doesn't exist in the YAML.
+        """
+        # Convert argparse Namespace to a standard dictionary if needed
+        args_dict = vars(args) if isinstance(args, argparse.Namespace) else args
+        
+        for key, val in args_dict.items():
+            # Only override if the key exists in YAML and the user provided a value
+            if key in self.params and val is not None:
+                self.params[key] = val
+                self.__setattr__(key, val)
+
